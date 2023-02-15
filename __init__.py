12b9cc9c6e949bc5530ac15b4b7bdecc93612765
roonsine <53196356+Roonsine@users.noreply.github.com>
2023-02-15 05:45:36 -0500

    Adjusted the imports, and added in update User

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from .webForms import IncomeForm, ExpenseForm, UserForm, ProductForm, NameForm
+import webForms as wf
 
 # Create the extension
 db = SQLAlchemy()
@@ -30,7 +30,7 @@ def AddIncome():
     incomeSource = None
     incomeAmount = None
     incomeDate = None
-    form = IncomeForm()
+    form = wf.IncomeForm()
     if form.validate_on_submit():
         if db.session.query(Income).filter_by(incomeAmount=incomeAmount):
             income = Income(incomeSource = form.incomeSource.data, incomeAmount = form.incomeAmount.data, incomeDate = form.incomeDate.data)
@@ -51,7 +51,7 @@ def AddExpense():
     expenseSource = None
     expenseAmount = None
     expenseDate = None
-    form = ExpenseForm()
+    form = wf.ExpenseForm()
     if form.validate_on_submit():
         if db.session.query(Expense).filter_by(expenseAmount=expenseAmount):
             expense = Expense(expenseSource = form.expenseSource.data, expenseAmount = form.expenseAmount.data, expenseDate = form.expenseDate.data)
@@ -72,7 +72,7 @@ def AddProduct():
     name = None
     price = None
     size = None
-    form = ProductForm()
+    form = wf.ProductForm()
     if form.validate_on_submit():
         if db.session.query(Products).filter_by(name=name):
             product = Products(name=form.name.data, price=form.price.data, size = form.size.data)         
@@ -89,8 +89,8 @@ def AddProduct():
     return render_template("products/add_product.html", form = form, name=name,our_products=our_products, price=price, size = size)
 
 @app.route('/product/update/<int:id>', methods=['GET', 'POST'])
-def UpdateProduct(id = None):
-    form = ProductForm()
+def UpdateProduct(id):
+    form = wf.ProductForm()
     productToUpdate = Products.query.get_or_404(id)
     if request.method == "POST":
         productToUpdate.name = request.form['name']
@@ -109,7 +109,7 @@ def UpdateProduct(id = None):
 @app.route('/user/add', methods=['GET', 'POST'])
 def AddUser(): 
     name = None
-    form = UserForm()
+    form = wf.UserForm()
     if form.validate_on_submit():
         if db.session.query(Users).filter_by(name=name):
             user = Users(name=form.name.data, email=form.email.data)
@@ -122,10 +122,27 @@ def AddUser():
     our_users = Users.query.order_by(Users.date_added)
     return render_template("user/add_user.html", name=name, form=form, our_users=our_users)
 
+@app.route('/user/update/<int:id>', methods=['GET', 'POST'])
+def UpdateUser(id):
+    form = wf.UserForm()
+    userToUpdate = Users.query.get_or_404(id)
+    if request.method == "POST":
+        userToUpdate.name = request.form['name']
+        userToUpdate.email = request.form['email']
+        try:
+            db.session.commit()
+            flash('User updated successfully')
+            return render_template('user/updateUser.html', form=form, userToUpdate=userToUpdate)
+        except:
+            flash('Error occured, please try again')
+            return render_template('user/updateUser.html', form=form, userToUpdate=userToUpdate)
+    else:
+        return render_template('user/updateUser.html', form=form, userToUpdate=userToUpdate)
+        
 @app.route("/name", methods=['GET', 'POST'])
 def name():
     name = None
-    form = NameForm()
+    form = wf.NameForm()
     # Validate form
     if form.validate_on_submit():
         name = form.name.data
